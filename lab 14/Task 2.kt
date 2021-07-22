4b931aaefcf8cf231147503453f679c1555a0089
Robert-zar <78647659+Robert-zar@users.noreply.github.com>
2021-07-22 18:28:02 +0300

    Add files via upload

---
+++ lab 14/Task 2.kt
@@ -0,0 +1,21 @@
+fun main(){
+    val number = Inputnumber();
+    println("Сумма цифр числа равна: ${sumOfDigitsTail(number)}")
+}
+fun Inputnumber(): Int {
+    print("Введите число:")
+    val number = readLine()!!.toString()
+    return try {
+        number.toInt()
+    } catch (e: NumberFormatException) {
+        Inputnumber()
+    }
+}
+fun sumOfDigitsTail(number: Int, sum: Int): Int =
+    if (number / 10 == 0)
+sum + (number % 10)
+else
+sumOfDigitsTail(number / 10, sum + (number % 10))
+
+fun sumOfDigitsTail(number: Int): Int =
+    sumOfDigitsTail(number, 0) 
