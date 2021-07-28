8cd7d9ae4394f47ad30738a60de1a17c535e1e09
Robert-zar <78647659+Robert-zar@users.noreply.github.com>
2021-07-28 13:30:14 +0300

    Add files via upload

---
+++ lab 15/Task 4.17.kt
@@ -0,0 +1,56 @@
+import java.util.*
+fun main(){
+    val input = Scanner(System.`in`)
+    val mas = readMass()
+    println("Изменённый массив:")
+    printMass(varMinMax(mas))
+}
+
+fun SearchNum(mas: IntArray, inVal: Int, j: Int = 0): Int =
+    if (j < mas.size) {
+        if (mas[j] == inVal)
+            j
+        else SearchNum(mas, inVal, j + 1)
+    } else inVal
+
+fun varMinMax(mas: IntArray): IntArray {
+    val inMin = SearchNum(mas, minDigit(mas))
+    val inMax = SearchNum(mas, maxDigit(mas))
+    var a = 0
+    a = mas[inMax]
+    mas[inMax] = mas[inMin]
+    mas[inMin] = a
+    return mas
+}
+
+ fun arrayOp(mas: IntArray, f: (Int, Int) -> Int, inVal: Int, i: Int = 0): Int =
+    if (i < mas.size) {
+        val a = f(inVal, mas[i])
+        arrayOp(mas, f, a, i + 1)
+    } else inVal
+
+fun maxDigit(mas: IntArray): Int =
+    arrayOp(mas, { max, x: Int -> if (x > max) x else max }, mas[0])
+
+fun minDigit(mas: IntArray): Int =
+    arrayOp(mas, { min, x: Int -> if (x < min) x else min }, mas[0])
+
+fun readMass(): IntArray {
+    val input = Scanner(System.`in`)
+    print("Введите размер массива: ")
+    val n = input.nextInt()
+    val mas = IntArray(n)
+    println("Введите элементы массива: ")
+    var i = 0
+    while (i < n) {
+        mas[i] = input.nextInt()
+        i++
+    }
+    return mas
+}
+
+fun printMass(mas: IntArray, i: Int = 0): String =
+    if (i != mas.size) {
+        println(mas[i])
+        printMass(mas, i + 1)
+    } else "\n"
