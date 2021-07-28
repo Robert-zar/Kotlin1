d2185930643ce21aff8f41c8c5c3a4eac083559b
Robert-zar <78647659+Robert-zar@users.noreply.github.com>
2021-07-28 15:14:14 +0300

    Add files via upload

---
+++ lab 15/Task 4.29.kt
@@ -0,0 +1,46 @@
+ import java.util.*
+fun main(){
+    val mas = readMass()
+    println("Введите а: ")
+    val input = Scanner(System.`in`)
+    val a = input.nextInt()
+    println("Введите b: ")
+    val b = input.nextInt()
+    println("Ваш максимум ${checkMax(mas, a,b)}")
+}
+fun checkMax(mas: IntArray, a: Int, b: Int): String {
+    val inMax = SearchNum(mas, maxDigit(mas))
+    if (inMax in a..b)
+        return "входит в промежуток"
+    return "не входит в промежуток"
+}
+
+fun SearchNum(mas: IntArray, inVal: Int, j: Int = 0): Int =
+    if (j < mas.size) {
+        if (mas[j] == inVal)
+            j
+        else SearchNum(mas, inVal, j + 1)
+    } else inVal
+
+fun readMass(): IntArray {
+    val input = Scanner(System.`in`)
+    print("Введите размер массива: ")
+    val n = input.nextInt()
+    val mas = IntArray(n)
+    println("Введите элементы массива: ")
+    var i = 0
+    while (i < n) {
+        mas[i] = input.nextInt()
+        i++
+    }
+    return mas
+}
+
+fun arrayOp(mas: IntArray, f: (Int, Int) -> Int, accum: Int, i: Int = 0): Int =
+    if (i < mas.size) {
+        val a = f(accum, mas[i])
+        arrayOp(mas, f, a, i + 1)
+    } else accum
+
+fun maxDigit(mas: IntArray): Int =
+    arrayOp(mas, { max, x: Int -> if (x > max) x else max }, mas[0]) 
